ae69395ade9885ddddcab2b88c84ea33722599da
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2015-03-17 08:33:08 +0100

    Prepare cli options

--- lib/volay/cli.rb
+++ lib/volay/cli.rb
@@ -19,7 +19,26 @@ module Volay
            short: '-l LEVEL',
            long: '--log_level LEVEL',
            description: 'Set the log level (debug, info, warn, error, fatal)',
+           in: %w(debug info warn error fatal),
            default: :info,
            proc: proc { |l| l.to_sym })
+
+    option(:up,
+           short: '-u PERCENT',
+           long: '--up PERCENT',
+           description: 'Up volume',
+           proc: proc { |l| l.to_i })
+
+    option(:down,
+           short: '-d PERCENT',
+           long: '--down PERCENT',
+           description: 'Down volume',
+           proc: proc { |l| l.to_i })
+
+    option(:muted,
+           short: '-m',
+           long: '--muted',
+           boolean: true,
+           description: 'Mute volume')
   end
 end
